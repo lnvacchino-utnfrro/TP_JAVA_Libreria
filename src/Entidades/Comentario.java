3df98ff9f6ffa0543fb2ef13e166d4341b2c5b95
yo <yo@DESKTOP-TUCBJA0>
2018-08-23 18:17:51 -0300

    y todavia mas clases

--- src/Entidades/Comentario.java
+++ src/Entidades/Comentario.java
@@ -5,5 +5,20 @@ import java.util.Date;
 public class Comentario {
 	private String descripcion;
 	private Date fechaCreacion;
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	
+	
 
 }
